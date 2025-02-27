69ebf2bb345ef3f4c4094392599c4a3a9c0e6929
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-02-27 22:58:45 +0900

    .

--- src/main/java/project/loadbalancing/TestController.java
+++ src/main/java/project/loadbalancing/TestController.java
@@ -1,4 +1,4 @@
-package project.roadbalancing;
+package project.loadbalancing;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 @RestController
 public class TestController {
-    // 로드밸런싱으로 서버가 바뀌면 UUID 응답도 변하도록
+    // 로드밸런싱으로 서버가 바뀌면 UUID로 응답도 변하도록
     public static final UUID randomUUID = UUID.randomUUID();
 
     @GetMapping("/test")
@@ -16,24 +16,27 @@ public class TestController {
         return ResponseEntity.ok(randomUUID);
     }
 
+    // 로컬 실행시
     //$ ./gradlew clean build -x test
     //$ docker compose up --build -d
     // 스프링 build 후 -> 도커 build
 
-    // 현재 방법은 같은 스프링을 빌드해서 1개의 이미지를 3개 컨테이너로 복붙 나누어서 로드 밸런싱
+    // 현재 방법은 1개의 스프링부트 빌드한 jar를 3개의 이미지(컨테이너)로 복붙 나누어서 로드 밸런싱
+    // 스프링 프로필을 달리하고 Dockerfile .active=프로필 따로 줘서 별개로 만들어서 올릴수도 있음
 
     // nginx upstream의 server 주소를
-    // 1. '컨테이너이름:컨테이너포트번호'로 연결할지
-    // 2. '호스트주소:호스트포트번호'로 연결할지에 따라 도커 ports 연결이 달라짐
+    // 1. '컨테이너이름:컨테이너포트번호'로 연결할지 [컨테이너 포트번호는 중복되도 별개로 존재]
+    // 2. '호스트주소:호스트포트번호'로 연결할지에 따라 컴포즈 ports 작성이 달라짐
 
     // 1번 방식은
     // ports:
-    //  - 의미없음:8080
+    //  - 아무거나:8080
     // 혹은
     // expose:
     //  - 8080
+    // 다만 expose는 도커 컨테이너 내부에서만 포트번호로 접근 가능
 
-    // 2번 방식은 ports
+    // 2번 방식은
     // ports:
     //  - 8080:8080
     // ports:
